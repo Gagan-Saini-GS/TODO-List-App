855f868d56f9819eb7d7e0fbf9c517c09f5c364a
Gagan Saini <gagansaini10999@gmail.com>
2022-09-15 16:27:40 +0530

    Adding all the styles.

--- app.js
+++ app.js
@@ -4,25 +4,26 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 // Array to store todo tasks or items
-var items = [];
+let items = [];
 
 app.get("/", function (req, res) {
-  var today = new Date();
-  var options = {
+  let today = new Date();
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
   res.render("list", { kindOfDay: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
-  var item = req.body.listItem;
+  let item = req.body.listItem;
   items.push(item);
   res.redirect("/");
 });
