1c54455e5300e811c528c5ee684310c3cce6f151
Gagan Saini <gagansaini10999@gmail.com>
2022-09-21 16:49:10 +0530

    Add database to project

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+// const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -13,28 +15,125 @@ app.set("view engine", "ejs");
 // Such like I can push elements in array but I can't assign anything new to this array.
 // For more explaination search on const MDN web docs.
 
-const items = [];
-const workItems = [];
+// const items = [];
+// const workItems = [];
 
-app.get("/", function (req, res) {
-  const day = date.getDate();
-  res.render("list", { listTitle: day, newListItems: items });
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = new mongoose.Schema({
+  name: String,
 });
 
-app.get("/work", function (req, res) {
-  res.render("list", { listTitle: "Work List", newListItems: workItems });
+const listSchema = new mongoose.Schema({
+  name: String,
+  items: [itemsSchema],
+});
+
+const Item = new mongoose.model("item", itemsSchema);
+const List = new mongoose.model("List", listSchema);
+
+const first = new Item({
+  name: "The first Task",
+});
+
+const second = new Item({
+  name: "The second Task",
+});
+
+const third = new Item({
+  name: "The third Task",
+});
+
+const defaultItems = [first, second, third];
+
+app.get("/", function (req, res) {
+  Item.find({}, function (err, foundItems) {
+    // To insert for first time
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully added!");
+        }
+      });
+    }
+
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+    }
+  });
+});
+
+app.get("/:listName", function (req, res) {
+  const listName = _.capitalize(req.params.listName);
+
+  List.findOne({ name: listName }, function (err, foundList) {
+    if (!err) {
+      if (!foundList) {
+        const list = new List({
+          name: listName,
+          items: defaultItems,
+        });
+
+        list.save();
+        res.redirect("/" + listName);
+      } else {
+        res.render("list", {
+          listTitle: foundList.name,
+          newListItems: foundList.items,
+        });
+      }
+    }
+  });
 });
 
 app.post("/", function (req, res) {
-  const item = req.body.listItem;
-  const list = req.body.list;
+  const itemName = req.body.listItem;
+  const listName = req.body.list;
 
-  if (list === "Work List") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
+  const newItem = new Item({
+    name: itemName,
+  });
+
+  if (listName === "Today") {
+    newItem.save();
     res.redirect("/");
+  } else {
+    List.findOne({ name: listName }, function (err, foundList) {
+      foundList.items.push(newItem);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
+});
+
+app.post("/delete", function (req, res) {
+  // console.log(req.body.checkbox);
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === "Today") {
+    Item.deleteOne({ _id: checkedItemId }, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Item Deleted");
+      }
+    });
+    res.redirect("/");
+  } else {
+    List.findOneAndUpdate(
+      { name: listName },
+      { $pull: { items: { _id: checkedItemId } } },
+      function (err, foundList) {
+        if (!err) {
+          res.redirect("/" + listName);
+        }
+      }
+    );
   }
 });
 
